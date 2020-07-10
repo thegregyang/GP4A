7503de10254d9859c0436d67921da083ac54816f
Michael Santacroce <misantac@microsoft.com>
2020-07-10 14:55:40 -0700

    cleanup

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
-
 
 def getCor(cov):
     d = np.diag(cov)**-0.5
@@ -10,21 +8,20 @@ def getCor(cov):
 def seqs2cov(inputs):
     inputs = np.array(inputs)
     inputcov = inputs @ inputs.T / len(inputs[0])
-
+    
     inputidxs = [0]
     for seq in inputs:
         inputidxs.append(inputidxs[-1] + len(seq))
     inputidxs.pop()
     return inputs, inputcov, inputidxs
-
-
+	
 def VStep(cov):
     '''
     Computes E[step(z) step(z)^T | z ~ N(0, `cov`)]
     where step is the function takes positive numbers to 1 and
     all else to 0, and 
     z is a multivariate Gaussian with mean 0 and covariance `cov`
-
+    
     Inputs:
         `cov`: An array where the last 2 dimensions contain covariance matrix of z (and the first dimensions are "batch" dimensions)
     Output:
@@ -36,12 +33,11 @@ def VStep(cov):
     c = d[..., None]**(-1) * cov * d[..., None, :]**(-1)
     return 2./np.pi * np.arcsin(np.clip(c, -1, 1))
 
-
 def VErf(cov):
     '''
     Computes E[erf(z) erf(z)^T | z ~ N(0, `cov`)]
     where z is a multivariate Gaussian with mean 0 and covariance `cov`
-
+    
     Inputs:
         `cov`: An array where the last 2 dimensions contain covariance matrix of z (and the first dimensions are "batch" dimensions)
     Output:
@@ -50,17 +46,16 @@ def VErf(cov):
     '''
     ll = list(range(cov.shape[-1]))
     d = np.sqrt(cov[..., ll, ll] + 0.5)
-
+    
     c = d[..., None]**(-1) * cov * d[..., None, :]**(-1)
     return 2./np.pi * np.arcsin(np.clip(c, -1, 1))
-
 
 def VDerErf(cov):
     '''
     Computes E[erf'(z) erf'(z)^T | z ~ N(0, `cov`)]
     where erf' is the derivative of erf and
     z is a multivariate Gaussian with mean 0 and covariance `cov`
-
+    
     Inputs:
         `cov`: An array where the last 2 dimensions contain covariance matrix of z (and the first dimensions are "batch" dimensions)
     Output:
@@ -72,12 +67,10 @@ def VDerErf(cov):
     dd = 1 + 2 * d
     return 4/np.pi * (dd[..., None] * dd[..., None, :] - 4 * cov**2)**(-1./2)
 
-
 def J1(c, eps=1e-10):
     c[c > 1-eps] = 1-eps
     c[c < -1+eps] = -1+eps
     return (np.sqrt(1-c**2) + (np.pi - np.arccos(c)) * c) / np.pi
-
 
 def VReLU(cov, eps=1e-5):
     ll = list(range(cov.shape[-1]))
@@ -85,7 +78,7 @@ def VReLU(cov, eps=1e-5):
     c = d[..., None]**(-1) * cov * d[..., None, :]**(-1)
     return np.nan_to_num(0.5 * d[..., None] * J1(c, eps=eps) * d[..., None, :])
 
-
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 def colorbar(mappable):
     ax = mappable.axes
     fig = ax.figure
